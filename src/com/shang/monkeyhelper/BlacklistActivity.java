f39aa3f59ace6ddfa8b781b22b47e5054bf6fd72
Wei Shang <1012691657@qq.com>
2017-11-02 13:54:17 +0800

    Signed-off-by: Wei Shang <1012691657@qq.com>

--- src/com/shang/monkeyhelper/BlacklistActivity.java
+++ src/com/shang/monkeyhelper/BlacklistActivity.java
@@ -49,8 +49,7 @@ public class BlacklistActivity extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		switch (id) {
+		switch (item.getItemId()) {
 		case android.R.id.home:
 			finish();
 			break;
--- src/com/shang/monkeyhelper/FloatWindowManager.java
+++ src/com/shang/monkeyhelper/FloatWindowManager.java
@@ -1,39 +1,54 @@
 package com.shang.monkeyhelper;
 
-import android.view.Display;
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
 
-public class FloatWindowManager implements WindowManager {
+public class FloatWindowManager {
 
-	@Override
-	public void addView(View view, android.view.ViewGroup.LayoutParams params) {
-		// TODO Auto-generated method stub
+	private static FloatView floatView;
 
+	private static LayoutParams floatViewParams;
+
+	private static WindowManager mWindowManager;
+
+	public static void createFloatView(Context context) {
+		WindowManager windowManager = getWindowManager(context);
+		DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+		int screenWidth = displayMetrics.widthPixels;
+		int screenHeight = displayMetrics.heightPixels;
+		if (floatView == null) {
+			floatView = (FloatView) View.inflate(context, R.layout.view_float, null).findViewById(R.id.view_float);
+			if (floatViewParams == null) {
+				floatViewParams = new LayoutParams();
+				floatViewParams.x = screenWidth / 2 - floatView.getViewWidth() / 2;
+				floatViewParams.y = screenHeight / 2 - floatView.getViewHeight() / 2;
+				floatViewParams.type = LayoutParams.TYPE_TOAST;
+				floatViewParams.format = PixelFormat.RGBA_8888;
+				floatViewParams.gravity = Gravity.START | Gravity.TOP;
+				floatViewParams.width = floatView.getViewWidth();
+				floatViewParams.height = floatView.getViewHeight();
+			}
+			windowManager.addView(floatView, floatViewParams);
+		}
 	}
 
-	@Override
-	public void updateViewLayout(View view, android.view.ViewGroup.LayoutParams params) {
-		// TODO Auto-generated method stub
-
+	public static void removeFloatView(Context context) {
+		if (floatView != null) {
+			WindowManager windowManager = getWindowManager(context);
+			windowManager.removeView(floatView);
+			floatView = null;
+		}
 	}
 
-	@Override
-	public void removeView(View view) {
-		// TODO Auto-generated method stub
-
+	private static WindowManager getWindowManager(Context context) {
+		if (mWindowManager == null) {
+			mWindowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+		}
+		return mWindowManager;
 	}
-
-	@Override
-	public Display getDefaultDisplay() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void removeViewImmediate(View view) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
